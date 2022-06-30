a0b6c9f7958058bad564b4b9fd705def2ffc2cba
Yura Filatov <yura.fila@gmail.com>
2022-06-30 13:54:25 -0400

    Initial Commit

---
+++ Shared/MindValleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindValleyApp.swift
+//  Shared
+//
+//  Created by Yura on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MindValleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
